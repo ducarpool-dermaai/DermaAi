93ff4b7226d5b649f16d12e6edda4ab70b9097b0
ducarpool-dermaai <ducarpool@gmail.com>
2026-03-20 19:23:41 +0530

    Add files via upload

---
+++ api/test.js
@@ -0,0 +1,8 @@
+module.exports = function handler(req, res) {
+  res.status(200).json({ 
+    status: "ok", 
+    message: "API is working!",
+    hasKey: !!process.env.ANTHROPIC_API_KEY,
+    keyPrefix: process.env.ANTHROPIC_API_KEY ? process.env.ANTHROPIC_API_KEY.substring(0, 10) + "..." : "NOT SET"
+  });
+};
